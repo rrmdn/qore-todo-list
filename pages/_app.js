c050a285cded1101183002c3634d10195ce77da2
Rizki Romadhoni <rrmdn@pm.me>
2021-01-07 18:19:43 +0700

    init todo list

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="stylesheet"
+          href="https://cdn.jsdelivr.net/npm/todomvc-app-css@2.3.0/index.css"
+        />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
---
+++ pages/todo.js
@@ -0,0 +1,70 @@
+import React from "react";
+import qoreContext from "../qoreContext";
+
+export default function Todo() {
+  const items = qoreContext.views.todoDefaultView.useListRow();
+  const {
+    insertRow,
+    status,
+  } = qoreContext.views.todoDefaultView.useInsertRow();
+  const inputRef = React.useRef(null);
+  const { deleteRow } = qoreContext.views.todoDefaultView.useDeleteRow();
+  const { updateRow } = qoreContext.views.todoDefaultView.useUpdateRow();
+  return (
+    <>
+      <section class="todoapp">
+        <header class="header">
+          <h1>todos</h1>
+          <input
+            ref={inputRef}
+            onKeyPress={async (e) => {
+              if (e.key === "Enter" && status !== "loading") {
+                await insertRow({
+                  name: e.currentTarget.value,
+                  description: "",
+                  done: false,
+                });
+                items.revalidate();
+                inputRef.current.value = "";
+              }
+            }}
+            class="new-todo"
+            placeholder="What needs to be done?"
+            autofocus
+          />
+        </header>
+        <section class="main">
+          <ul class="todo-list">
+            {(items.data || []).map((item) => (
+              <li key={item.id} class={item.done && "completed"}>
+                <div class="view">
+                  <input
+                    onChange
+                    class="toggle"
+                    type="checkbox"
+                    checked={item.done}
+                    onChange={async (e) => {
+                      await updateRow(item.id, {
+                        done: e.currentTarget.checked,
+                      });
+                      items.revalidate();
+                    }}
+                  />
+                  <label>{item.name}</label>
+                  <button
+                    class="destroy"
+                    onClick={async (e) => {
+                      await deleteRow(item.id);
+                      items.revalidate();
+                    }}
+                  ></button>
+                </div>
+                <input class="edit" value="Create a TodoMVC template" />
+              </li>
+            ))}
+          </ul>
+        </section>
+      </section>
+    </>
+  );
+}
